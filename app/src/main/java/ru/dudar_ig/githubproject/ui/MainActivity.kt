5d994aaf1b856234a788adc4752c00715334bd78
DudarIg <dudar.ig@yandex.ru>
2022-01-27 07:22:21 +0300

    packet

--- app/src/main/java/ru/dudar_ig/githubproject/ui/MainActivity.kt
+++ app/src/main/java/ru/dudar_ig/githubproject/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.dudar_ig.githubproject
+package ru.dudar_ig.githubproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dudar_ig.githubproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
