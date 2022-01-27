6aa5037befcffa7da055932d7782a5a76269ba94
DudarIg <dudar.ig@yandex.ru>
2022-01-27 07:43:39 +0300

    binding

--- app/src/main/java/ru/dudar_ig/githubproject/ui/MainActivity.kt
+++ app/src/main/java/ru/dudar_ig/githubproject/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package ru.dudar_ig.githubproject.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.dudar_ig.githubproject.R
+import ru.dudar_ig.githubproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
