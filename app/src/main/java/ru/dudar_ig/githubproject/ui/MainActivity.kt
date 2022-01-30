5b77e55d378b3776f42740874eee0106bf27a297
DudarIg <dudar.ig@yandex.ru>
2022-01-30 13:03:47 +0300

    binding1

--- app/src/main/java/ru/dudar_ig/githubproject/ui/MainActivity.kt
+++ app/src/main/java/ru/dudar_ig/githubproject/ui/MainActivity.kt
@@ -2,11 +2,10 @@ package ru.dudar_ig.githubproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.dudar_ig.githubproject.R
 import ru.dudar_ig.githubproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit private var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
